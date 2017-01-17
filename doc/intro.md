3ed884d1929213c024b09fc2fcef73c3ca2be48a
pkova <pyry.kovanen@gmail.com>
2017-01-17 19:56:51 +0200

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-poker
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojure_poker/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-poker.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-poker.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

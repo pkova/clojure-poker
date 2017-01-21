36efdd7165a80314e0ffa3a2d5efdb5ca9e3b29e
Hannula <ripperi@users.noreply.github.com>
2017-01-21 03:22:24 +0200

    Implement deal function

--- src/clojure_poker/core.clj
+++ src/clojure_poker/core.clj
@@ -5,7 +5,7 @@
   {:players []
    :pot 0
    :biggest-bet 0
-   ;; :deck (shuffle eval/deck)
+   :deck (shuffle eval/deck)
    :board []
    :big-blind nil
    :small-blind nil
@@ -15,10 +15,22 @@
   {:name name
    :hand []
    :chips chips
-   :bet 0})
+   :bet 0
+   :sit-out true
+   :folded false})
 
 (defn add-player [game player]
   (update game :players #(conj % player)))
 
 (defn remove-player [game player]
   (update game :players #(vec (remove (partial = player) %))))
+
+(defn deal
+  ([game]
+   (deal game 2))
+  ([game hand-size]
+   (let [player-count (count (:players game))
+         hands (map vec (partition hand-size (take (* hand-size player-count) (:deck game))))]
+     (-> (update game :players (fn [xs] (map-indexed #(assoc %2 :hand (nth hands %1)) xs)))
+         (assoc :deck (drop (* hand-size player-count) (:deck game)))))))
+

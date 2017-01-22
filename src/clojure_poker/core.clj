b38ac79fb07029a415c4d45be6d74c8e77068c5b
Hannula <ripperi@users.noreply.github.com>
2017-01-22 03:51:16 +0200

    Implement initial betting logic

--- src/clojure_poker/core.clj
+++ src/clojure_poker/core.clj
@@ -5,11 +5,13 @@
   {:players []
    :pot 0
    :biggest-bet 0
+   :last-raise 0
    :deck (shuffle eval/deck)
    :board []
    :big-blind nil
    :small-blind nil
-   :turn nil})
+   :turn nil
+   :last-player nil})
 
 (defn player [name chips]
   {:name name
@@ -33,4 +35,51 @@
          hands (map vec (partition hand-size (take (* hand-size player-count) (:deck game))))]
      (-> (update game :players (fn [xs] (map-indexed #(assoc %2 :hand (nth hands %1)) xs)))
          (assoc :deck (drop (* hand-size player-count) (:deck game)))))))
+
+(defn turn? [game player]
+  (= (:turn game) player))
+
+(defn can-raise? [game player total]
+  (and
+   (turn? game player)
+   (>= (:chips player) (- total (:bet player)))
+   (<= (+ (:last-raise game) (:biggest-bet game)) total)))
+
+(defn can-call? [game player]
+  (and
+   (turn? game player)
+   (>= (:chips player) (:biggest-bet game))))
+
+(defn can-check? [game player]
+  (and
+   (turn? game player)
+   (= (:bet player) (:biggest-bet game))))
+
+(defn- bet [game player total]
+  (let [i (.indexOf (:players game) player)
+        amount (- total (:bet player))
+        updated-player (-> (update player :chips - amount)
+                           (assoc :bet total))
+        updated-game (-> (update game :pot + amount)
+                         (assoc-in [:players i] updated-player))]
+    (if (< (:biggest-bet game) total)
+      (-> (assoc updated-game :biggest-bet total)
+          (assoc :last-raise amount))
+      updated-game)))
+
+(defn raise [game player total]
+  (if (can-raise? game player total)
+    (bet game player total)
+    false))
+
+(defn call [game player]
+  (if (can-call? game player)
+    (bet game player (:biggest-bet game))
+    false))
+
+(defn check [game player]
+  (if (can-check? game player)
+    (???)
+    false))
+
 
